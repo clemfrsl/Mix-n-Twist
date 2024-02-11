42a5ab13eb4d9db26c73856e7fffdd4d9acfecab
clemf <clem.fresnel@gmail.com>
2024-02-11 22:27:48 +0100

    faire filtre

--- src/src/global/Filtre.java
+++ src/src/global/Filtre.java
@@ -1,4 +1,29 @@
 package global;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Filtre {
+
+    private List<Contrainte> contraintes;
+
+    public Filtre(List<Contrainte> contraintes) {
+        this.contraintes = contraintes;
+    }
+
+    public List<Recette> filtrer(List<Recette> recettes){
+        List<Recette> newListRecettes = new ArrayList<>();
+        for(Recette recette : recettes){
+            int count = -1;
+            for(Contrainte contrainte: contraintes){
+                if(recette.getContraintes().contains(contrainte)){
+                    count++;
+                }
+            }
+            if (count == -1){
+                newListRecettes.add(recette);
+            }
+        }
+        return newListRecettes;
+    }
 }
