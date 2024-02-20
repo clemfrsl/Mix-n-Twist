8012c90573ea01f40c9415bbe86c634257096422
clemf <clem.fresnel@gmail.com>
2024-02-20 23:51:57 +0100

    VICTOIRE mySQL connecter

--- src/src/DatabaseConnection.java
+++ src/src/DatabaseConnection.java
@@ -2,18 +2,23 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    public static void main( String[] args ) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        //create connection for a server installed in localhost, with a user "root" with no password
-        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/Mix-n-twist", "root", null)) {
-            // create a Statement
-            try (Statement stmt = conn.createStatement()) {
-                //execute query
-                try (ResultSet rs = stmt.executeQuery("SELECT 'Hello World!'")) {
-                    //position result to first
-                    rs.first();
-                    System.out.println(rs.getString(1)); //result is "Hello World!"
-                }
+        // Supprimez la ligne de chargement du pilote
+        // Class.forName("com.mysql.jdbc.Driver");
+
+        String url = "jdbc:mysql://localhost:3306/mix-n-twist";
+        String user = "M1";
+        String password = "mixntwist";
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String sql = "select nom from Ingredient where id_ingredient = 1;";
+        try (Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            // execute query
+            try (ResultSet rs = stmt.executeQuery(sql)) {
+                // position result to first
+                rs.first();
+                System.out.println(rs.getString(1)); // result is "Hello World!"
             }
         }
     }
