74543c2d4d63bcc9551ba0418b402b02d1115e23
alexa <alexandra.martin881@orange.fr>
2024-03-10 14:23:45 +0100

    AlcoolTest V1 alex

---
+++ src/src/test/AlcoolTest.java
@@ -0,0 +1,56 @@
+package test;
+
+
+//Les imports
+import static org.junit.Assert.assertEquals;
+
+import global.Contrainte;
+import ingredient.Alcool;
+import ingredient.Ingredient;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+
+
+
+public class AlcoolTest {
+
+    private Ingredient alcool;
+
+    //mise en place
+    @Before
+    public void setUp() {
+        alcool = new Alcool("Rhum", List.of(Contrainte.ALCOOLISE));
+    }
+
+
+
+
+    //recup nom
+    @Test
+    public void testGetNom() {
+        //(tout ok!)
+        assertEquals("Rhum", alcool.getNom());
+    }
+
+
+    //test getContraintes
+    @Test
+    public void testGetContraintes() {
+        //(tout ok !)
+        assertEquals(List.of(Contrainte.ALCOOLISE), alcool.getContraintes());
+    }
+
+
+
+    //verif qu'un alcool sans contrainte est quand meme un alcool
+    @Test
+    public void AlcoolSansContraintes() {
+        Ingredient alcoolSansContraintes = new Alcool("Vodka");
+        assertEquals("Vodka", alcoolSansContraintes.getNom());
+        assertEquals(List.of(Contrainte.ALCOOLISE), alcoolSansContraintes.getContraintes());
+    }
+}
+
